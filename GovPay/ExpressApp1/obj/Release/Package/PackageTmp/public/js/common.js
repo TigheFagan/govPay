309e47c3ac21c6af1f7996bd389a305edb903c0a
TigheFagan <tighe@gannettpeaktech.com>
2016-02-21 14:58:44 -0700

    publish and caching on load and calc

---
+++ GovPay/ExpressApp1/obj/Release/Package/PackageTmp/public/js/common.js
@@ -0,0 +1,9 @@
+﻿function nformatter(num) {
+    if (num >= 1000000) {
+        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + ' Million';
+    }
+    if (num >= 1000) {
+        return (num / 1000).toFixed(1).replace(/\.0$/, '') + ' Thousand';
+    }
+    return num;
+}
