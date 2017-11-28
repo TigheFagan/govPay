7893f8916f8a7274fb8e46583c1610168d963955
tighe <tighe@gannettpeaktech.com>
2017-11-28 14:56:38 -0700

    remove obj

--- GovPay/ExpressApp1/obj/Release/Package/PackageTmp/public/js/common.js
+++
@@ -1,9 +0,0 @@
-﻿function nformatter(num) {
-    if (num >= 1000000) {
-        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + ' Million';
-    }
-    if (num >= 1000) {
-        return (num / 1000).toFixed(1).replace(/\.0$/, '') + ' Thousand';
-    }
-    return num;
-}
